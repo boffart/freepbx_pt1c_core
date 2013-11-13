aab85042e5c239477c0e6fa2614184ef26862ce4
Boffart <boffart@gmail.com>
2013-11-13 21:29:09 +0400

    Исходная версия 2.11.0.6

---
+++ core/page.extensions.php
@@ -0,0 +1,44 @@
+<?php /* $Id: page.extensions.php 14523 2012-11-01 23:18:25Z p_lindheimer $ */
+//This file is part of FreePBX.
+//
+//    FreePBX is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    FreePBX is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with FreePBX.  If not, see <http://www.gnu.org/licenses/>.
+//    Copyright (C) 2004 Coalescent Systems Inc. (info@coalescentsystems.ca)
+//
+if (!defined('FREEPBX_IS_AUTH')) { die('No direct script access allowed'); }
+?>
+
+<div class="rnav">
+<?php 
+$extens = core_users_list();
+$description = _("Extension");
+drawListMenu($extens, $skip, $type, $display, $extdisplay, $description);
+?>
+	<br />
+</div>
+<?php
+// If this is a popOver, we need to set it so the selection of device type does not result
+// in the popover closing because config.php thinks it was the process function. Maybe
+// the better way to do this would be to log an error or put some proper mechanism in place
+// since this is a bit of a kludge
+//
+if (!empty($_REQUEST['fw_popover']) && empty($_REQUEST['tech_hardware'])) {
+?>
+	<script>
+		$(document).ready(function(){
+			$('[name="fw_popover_process"]').val('');
+			$('<input>').attr({type: 'hidden', name: 'fw_popover'}).val('1').appendTo('.popover-form');
+		});
+	</script>
+<?php
+}
---
+++ pack.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cd /var/www/html/admin/modules/
+tar -czf core-2.11.0.6.tgz core
